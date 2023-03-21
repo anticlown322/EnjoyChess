0909b027b7708cb9eec7e7628a812bc01ca4685e
karasandrey2005@gmail.com <anticlown322>
2023-03-22 00:01:16 +0300

    Change WelcomeWindow

--- Curse_ach/EnjoyChess.dpr
+++ Curse_ach/EnjoyChess.dpr
@@ -10,13 +10,16 @@ uses
   EnjoyChessSolver in 'EnjoyChessSolver.pas',
   EnjoyChessCheckMoves in 'EnjoyChessCheckMoves.pas',
   EnjoyChessSounds in 'EnjoyChessSounds.pas',
-  EnjoyChessVCLSettings in 'EnjoyChessVCLSettings.pas' {frmSettings};
+  EnjoyChessVCLSettings in 'EnjoyChessVCLSettings.pas' {frmSettings},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Smokey Quartz Kamri');
   Application.CreateForm(TfrmWelcomeWindow, frmWelcomeWindow);
   Application.CreateForm(TdtmdData, dtmdData);
   Application.Run;
